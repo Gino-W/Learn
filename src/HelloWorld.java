6a16d6208649901f31cced0c99eccb8f1d8180ca
Gino-W <180656245@qq.com>
2022-03-16 21:36:29 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author ：Gino
+ * @date ：Created in 2022/3/16 21:35
+ * @description：Test
+ * @modified By：
+ * @version: 1.0
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
